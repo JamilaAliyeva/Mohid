71d908ecdd88f56a2db8d9f11447518d3174952c
JamilaAliyeva <jaliyevaas@gmail.com>
2023-06-13 03:49:42 +0500

    sections

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// $(".owl-carousel").owlCarousel({
+//   loop: true,
+//   margin: 10,
+//   nav: true,
+//   responsive: {
+//     0: {
+//       items: 1,
+//     },
+//     600: {
+//       items: 3,
+//     },
+//     1000: {
+//       items: 5,
+//     },
+//   },
+// });
+const loading = document.getElementById("loading");
+setTimeout(() => {
+  loading.classList.add("loading-none");
+}, 2000);
+
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+    navbar.classList.remove("navbar-shrink");
+  }
+}
